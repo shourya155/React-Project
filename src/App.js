6a89d97f29ec669a190ffdc3e45710fc9c10f158
ravi230195 <shouryam@buffalo.edu>
2020-07-05 11:21:21 -0400

    styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,116 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import person from './Person/Person';
+import styled from 'styled-components';
 
+const StyledButt = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color:white;
+  border: 1x solid blue;
+  padding: 8px;
+  cursor:pointer;
+  font:inherit;
+  &:hover{
+    background-color: yellow;
+    color:black;
+  }
+`;
+class App extends Component{
+  state = {
+    person:[
+      {id:'11',name: 'Rohith', age:16},
+      {id:'14',name: 'Kauhik', age:17},
+      {id:'13',name: 'Vam sai', age:18},
+      {id:'12',name: 'Dilip', age:18}
+    ],
+    showPersons: false
+  }
+  clickingHandler = (newName) =>{
+    this.setState({
+      person:[
+      {name: 'Rohith', age:16},
+      {name: 'Kauhik', age:17},
+      {name: newName, age:28},
+      {name: 'Dilip', age:18}
+      ]
+    });
+  }
+  changevamsainame = (event) =>{
+    this.setState({
+      person:[
+      {name: 'Rohith', age:16},
+      {name: 'Kauhik', age:17},
+      {name: event.target.value, age:28},
+      {name: 'Dilip', age:18}
+      ]
+    });
+  }
+  dynamicnamechange = (event,idd) =>{
+    const tempindex = this.state.person.findIndex(p =>{
+      return p.id===idd;
+    });
+    const targetarra = {...this.state.person[tempindex]};
+    targetarra.name = event.target.value;
+    const persondup = [
+      ...this.state.person
+    ]
+    persondup[tempindex]=targetarra;
+    this.setState({
+      person:persondup
+    })
+  }
+  togglePersonsHandler = () =>{
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+  deletemeplease = (hindex) =>{
+    const persons = [...this.state.person];
+    persons.splice(hindex,1);
+    this.setState({person:persons});
+  }
+  render(){
+    
+    let flag = null;
+    
+    if(this.state.showPersons)
+    {
+      flag = ( 
+    <div>
+      {this.state.person.map((element,index) =>{
+        return <Person name={element.name} age={element.age} click={() => this.deletemeplease(index)} key={element.id} change ={(event) => this.dynamicnamechange(event,element.id)}> </Person>
+      }
+      )}
+    </div>
+      );
+    }
+    //style.backgroundColor='red'
+    //style[':hover']={
+    //  backgroundColor:'purple',
+    //  color:'pink'
+    //}
+    const dynamiccoloring = []
+    if(this.state.person.length<=2)
+    {
+      dynamiccoloring.push('red')
+    }
+    
+    if(this.state.person.length<=1)
+    {
+      dynamiccoloring.push('bold')
+    }
+  return(
+    
+    <div className="App">
+    <p className={dynamiccoloring.join(' ')}>this is working</p>
+    <StyledButt alt={this.state.showPersons} onClick={this.togglePersonsHandler}> Let us change age </StyledButt>
+    {flag}
+    </div>
+    
+  );
+}
+}
+
+/*
 const app = props => {
   const [originalstatus,newstatus] = useState(
     {
@@ -38,8 +146,8 @@ const app = props => {
     )
     //return (React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hello this is testing')));
 };
-
-export default app;
+*/
+export default App;
 /*
 class App extends Component {
 state = {
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import './Person.css';
+import styled from 'styled-components';
+const StyledDiv = styled.div`
+    width: 60%;
+    color:pink;
+    margin: auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    '@media(min-width:500px)':{
+        width:'450px'
+    }
+`;
 const person = (props) => {
+    
     return (
-    <div>
-    <h4> i am {props.name} of age {props.age} </h4>
+    <StyledDiv>
+    <h4 onClick={props.click}> i am {props.name} of age {props.age} </h4>
     <p>{props.children}</p>
-    </div>
+    <input type="text" onChange={props.change} value={props.name}/>
+    </StyledDiv>
     )
 };
 
